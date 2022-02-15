a07059b3906281e2714fb9f090856c4d1c9f7950
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-02-15 15:26:06 +0800

    added logo

--- views/includes/head/head.php
+++ views/includes/head/head.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/sweetalert2.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/images/arkLogoHD.png">
 
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
